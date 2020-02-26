2bd59a0c1aad9705edbc81e186a71b0739a586fa
anja <rasoloanja@gmail.com>
2020-02-26 17:50:02 -0500

    Updates

---
+++ precache-manifest.6b14ccd685058c89e46834a345540297.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84da595fef14982e64cd192894156ef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45cfd9631a7bf89be8bb",
+    "url": "/static/css/main.2d950961.chunk.css"
+  },
+  {
+    "revision": "070c0bac6718a66d03c1",
+    "url": "/static/js/2.786740cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.786740cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45cfd9631a7bf89be8bb",
+    "url": "/static/js/main.9f08cf84.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "5539c0204d1b95f6d20a52ade8497194",
+    "url": "/static/media/prof.5539c020.jpg"
+  },
+  {
+    "revision": "17a82619dbab161bcc7435f609d92ad6",
+    "url": "/static/media/sprite.17a82619.svg"
+  },
+  {
+    "revision": "36ed0ace77f1ebd9f00779921b35c47c",
+    "url": "/static/media/triano.36ed0ace.PNG"
+  },
+  {
+    "revision": "6d4394307fcee12d0c6a916a478e841c",
+    "url": "/static/media/trimmo.6d439430.PNG"
+  },
+  {
+    "revision": "7fc3f3aa5033db2ae96cfd07c203aa1f",
+    "url": "/static/media/weather.7fc3f3aa.PNG"
+  },
+  {
+    "revision": "804b1611c1a5fe8037bc13c1a07c5c19",
+    "url": "/static/media/woto-admin.804b1611.PNG"
+  },
+  {
+    "revision": "ff381b8c6299036d5768db56cb57856e",
+    "url": "/static/media/woto-client.ff381b8c.PNG"
+  }
+]);
